421cb9b0702f8135add4ebee703712437b279137
Pavanntej <pavanntej@gmail.com>
2026-02-25 18:39:36 +0530

    Implement Netflix hover preview for book elements
    Added hover preview functionality for Netflix books.

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function closeTrailer(){
 document.getElementById("modal").style.display="none";
 document.getElementById("frame").src="";
 }
+
+/* NETFLIX HOVER PREVIEW */
+document.querySelectorAll(".book").forEach(book=>{
+
+const videoId = book.dataset.video;
+const iframe = book.querySelector(".preview");
+
+book.addEventListener("mouseenter",()=>{
+
+iframe.src =
+`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${videoId}`;
+
+iframe.style.opacity=1;
+});
+
+book.addEventListener("mouseleave",()=>{
+
+iframe.src="";
+iframe.style.opacity=0;
+});
+
+});
